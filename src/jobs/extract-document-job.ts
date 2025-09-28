b68956df5be0bb898d7659d0ce3f89617460a419
Daimus Suudi <daimus.suudi@gmail.com>
2025-09-28 08:40:57 +0700

    chore: reformat job log

--- src/jobs/extract-document-job.ts
+++ src/jobs/extract-document-job.ts
@@ -27,71 +27,72 @@ export default function () {
 
     job.on('waiting', function (jobId) {
         // A Job is waiting to be processed as soon as a worker is idling.
-        Logger.info("%s | JOB %s > waiting", jobId, JOB_NAME);
+        Logger.info("JOB %s : %s > waiting", JOB_NAME, jobId);
     });
 
     job.on('active', function (job, jobPromise) {
         // A job has started. You can use `jobPromise.cancel()`` to abort it.
-        Logger.info("%s | JOB %s > running", job.id, JOB_NAME);
+        Logger.info("JOB %s : %s > active", JOB_NAME, job.id);
     })
 
     job.on('stalled', function (job) {
         // A job has been marked as stalled. This is useful for debugging job
         // workers that crash or pause the event loop.
-        Logger.info("%s | JOB %s > stalled", job.id, JOB_NAME);
+        Logger.info("JOB %s : %s > stalled", JOB_NAME, job.id);
     })
 
     job.on('lock-extension-failed', function (job, err) {
         // A job failed to extend lock. This will be useful to debug redis
         // connection issues and jobs getting restarted because workers
         // are not able to extend locks.
-        Logger.info("%s | JOB %s > lock-extension-failed : %j", job.id, JOB_NAME, err);
+        Logger.info("JOB %s : %s > lock-extension-failed : %j", JOB_NAME, job.id, err);
     });
 
     job.on('progress', function (job, progress) {
         // A job's progress was updated!
-        Logger.info("%s | JOB %s > progress : %s", job.id, JOB_NAME, progress);
+        Logger.info("JOB %s : %s > progress : %s", JOB_NAME, job.id, progress);
     })
 
     job.on('completed', function (job, result) {
         // A job successfully completed with a `result`.
-        Logger.info("%s | JOB %s > completed : %j", job.id, JOB_NAME, result);
+        Logger.info("JOB %s : %s > completed : %j", JOB_NAME, job.id, result);
     })
 
     job.on('failed', function (job, err) {
         // A job failed with reason `err`!
-        Logger.info("%s | JOB %s > failed : %j", job.id, JOB_NAME, err);
+        Logger.info("JOB %s : %s > failed : %j", JOB_NAME, job.id, err);
     })
 
     job.on('paused', function () {
         // The queue has been paused.
-        Logger.info("%s | JOB %s > paused", '', JOB_NAME);
+        Logger.info("JOB %s > paused", JOB_NAME);
     })
 
     job.on('resumed', function (job) {
         // The queue has been resumed.
-        Logger.info("%s | JOB %s > resumed", job.id, JOB_NAME);
+        Logger.info("JOB %s : %s > resumed", JOB_NAME, job.id);
     })
 
     job.on('cleaned', function (jobs, type) {
         // Old jobs have been cleaned from the queue. `jobs` is an array of cleaned
         // jobs, and `type` is the type of jobs cleaned.
-        Logger.info("%s | JOB %s > cleaned : %j", '', JOB_NAME, type);
+        Logger.info("JOB %s > cleaned : %j", JOB_NAME, type);
     });
 
     job.on('drained', function () {
         // Emitted every time the queue has processed all the waiting jobs (even if there can be some delayed jobs not yet processed)
-        Logger.info("%s | JOB %s > drained", '', JOB_NAME);
+        Logger.info("JOB %s > drained", JOB_NAME);
     });
 
     job.on('removed', function (job) {
         // A job successfully removed.
-        Logger.info("%s | JOB %s > removed", job.id, JOB_NAME);
+        Logger.info("JOB %s : %s > removed", JOB_NAME, job.id);
     });
     return job;
 }
 
 async function handler(job, done) {
+    Logger.info("JOB %s : %s > running", JOB_NAME, job.id);
     try {
         const fileServiceInstance = Container.get(FileService);
         const candidateServiceInstance = Container.get(CandidateService);
@@ -99,6 +100,7 @@ async function handler(job, done) {
         await candidateServiceInstance.CreateCandidate(job.data.candidateId, result);
         done(null)
     } catch (error) {
+        Logger.info("JOB %s : %s > error : %j", JOB_NAME, job.id, error);
         done(error);
     }
 }
