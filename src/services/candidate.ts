619e5f27a9f7b22ed9f2a043573fa257b21f744e
Daimus Suudi <daimus.suudi@gmail.com>
2025-09-29 10:46:40 +0700

    feat: add GetCandidate method and improve CreateCandidate logic

--- src/services/candidate.ts
+++ src/services/candidate.ts
@@ -1,6 +1,8 @@
 import { Inject, Service } from "typedi";
 import { EventDispatcher, EventDispatcherInterface, } from "@/decorators/eventDispatcher";
 import { candidateTable, experienceTable, projectTable } from "$/schema";
+import { eq } from "drizzle-orm";
+import { HTTPException } from "hono/http-exception";
 
 @Service()
 export default class CandidateService {
@@ -10,9 +12,20 @@ export default class CandidateService {
         @EventDispatcher() private eventDispatcher: EventDispatcherInterface
     ) { }
 
+    public async GetCandidate(id: string) {
+        this.logger.info("SVC CandidateService/GetCandidate %s", id);
+        return await this.db.query.candidateTable.findFirst({
+            where: eq(candidateTable.id, id),
+            with: {
+                experiences: true,
+                projects: true
+            }
+        });
+    }
+
     public async CreateCandidate(id, candidate) {
         this.logger.info("SVC CandidateService/CreateCandidate %s > %j", id, candidate);
-        await Promise.all([
+        const [[result]] = await Promise.all([
             this.db.insert(candidateTable).values({
                 id: id,
                 name: candidate.name,
@@ -20,7 +33,7 @@ export default class CandidateService {
                 summaryProfile: candidate.summaryProfile,
                 skills: candidate.skills,
                 softSkills: candidate.softSkills,
-            }),
+            }).returning({ id: candidateTable.id }),
             this.db.insert(experienceTable).values(candidate.experiences.map(experience => {
                 return {
                     candidateId: id,
@@ -33,6 +46,12 @@ export default class CandidateService {
                     ...project
                 }
             })),
-        ])
+        ]);
+        if (result?.id) {
+            return await this.GetCandidate(result.id);
+        }
+        throw new HTTPException(500, {
+            message: "Failed to create candidate."
+        })
     }
 }
